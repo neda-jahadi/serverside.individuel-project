8503b312ee1f61dcdae5cea7b35b7edec2d8f8eb
Neda Jahadi <neda_jahadi@yahoo.com>
2020-09-03 09:54:32 +0200

    ta bort comments

--- server.js
+++ server.js
@@ -20,15 +20,7 @@ app.use(express.static(__dirname + '/frontend'));
 app.use( bodyParser.urlencoded({ extended: true }) );
 app.use( bodyParser.json() );
 
-let data = [
-    { modelname:'Aluminum Ribs', production:2015, price:1200.40, sailboat:'yes', motor:'yes'},
-    { modelname:'Inflatable Rescue', production:2019, price:7600.40, sailboat:'no', motor:'yes'},
-    { modelname:'Fiberglass Fishing Panga', production:1998, price:4300.40, sailboat:'no', motor:'yes'},
-    { modelname:'Foldable Inflatable', production:2000, price:3800.42, sailboat:'no', motor:'yes'},
-    { modelname:'Inflatable Catamaran', production:2011, price:2300.40, sailboat:'no', motor:'yes'},
-    { modelname:'Fishing and Banana', production:2005, price:1300.90, sailboat:'no', motor:'yes'},
-    { modelname:'Fiberglass Hulls', production:2013, price:5300.80, sailboat:'no', motor:'yes'}
-]
+
 
 app.get('/api/boats', (req,res)=>{
     getAllBoats(dataOrError =>{
@@ -76,8 +68,7 @@ app.post('/api/boat', (req,res)=>{
   insertBoat(newBoat, dataOrError => {
     res.send(dataOrError)
   })
-  // data.push(newBoat);
-  // res.send('New boat added');
+  
 })
 
 app.delete('/api/boatdelete', (req,res)=>{
@@ -85,8 +76,7 @@ app.delete('/api/boatdelete', (req,res)=>{
     deletedBoat(req.query.id, dataOrError => {
       res.send(dataOrError)
     })
-    // data = data.filter(boat=>boat.modelname!==deletedBoat);
-    // res.send(`${deletedBoat} is deleted`);
+    
 })
 
 
